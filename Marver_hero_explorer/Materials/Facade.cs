7458f31790ffb32adafb564bc994bad5294af452
grayevilserg <grayevilserg@mail.ru>
2018-06-22 16:29:20 +0300

    creating class Facade

---
+++ Marver_hero_explorer/Materials/Facade.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace Marver_hero_explorer.Materials
+{
+    public class Facade
+    {
+        private const string PrivateKey = "cd1cd6d81af8ccbd5bc275d410bda34bc747285e";
+        private const string PublicKey = "eaf0958022fd8c5a5c079f58349ad7e7";
+        private const int MaxChars = 500;
+        private const string ImageNotAvailablePath = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+
+        private static string CalcDM5(string str)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+
+            return res;
+        }
+
+        private static string CreateHash(string TimeStamp)
+        {
+
+            var ToHashed = TimeStamp + PrivateKey + PublicKey;
+            var HashedMessage = CalcDM5(ToHashed);
+
+            return HashedMessage;
+        }
+
+        private static async Task<CharacterDataWrapper> GetCharacterDataWrapperAsync()
+        {
+            var TimeStamp = DateTime.Now.Ticks.ToString();
+            Random Rand = new Random();
+            var Offset = 10;//Rand.Next(1, MaxChars);
+
+            //Get MD5 hash
+
+            var Hash = CreateHash(TimeStamp);
+
+            //Creating url
+
+            string Url = string.Format("https://gateway.marvel.com/v1/public/characters?limit=1&offset={0}&ts={1}&apikey={2}&hash={3}", Offset, TimeStamp, PublicKey, Hash);
+            
+            //Call to Marvel side, creation josn object
+
+            HttpClient Http = new HttpClient();
+            var Response = await Http.GetAsync(Url);
+            var JsonMessage = await Response.Content.ReadAsStringAsync();
+
+            //Deserialize
+
+            var serializer = new DataContractJsonSerializer(typeof(CharacterDataWrapper));
+            var MS = new MemoryStream(Encoding.UTF8.GetBytes(JsonMessage));
+            
+            var result = (CharacterDataWrapper)serializer.ReadObject(MS);
+            return result;
+        }
+    }
+}
