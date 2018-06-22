b4c1a3501ecf10355ad2cc51140fc2601a0142d1
grayevilserg <grayevilserg@mail.ru>
2018-06-22 16:53:11 +0300

    Created collection of characters and created event of load page

--- Marver_hero_explorer/MainPage.xaml.cs
+++ Marver_hero_explorer/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -7,6 +8,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Marver_hero_explorer.Materials;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -22,14 +24,24 @@ namespace Marver_hero_explorer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<Character> MarvelCharacters{ get; set;}
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            MarvelCharacters = new ObservableCollection<Character>();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            MyRing.IsActive = true;
+            MyRing.Visibility = Visibility.Visible;
 
+            await Facade.PopulateMarvelCharactersAsync(MarvelCharacters);
+
+            MyRing.IsActive = false;
+            MyRing.Visibility = Visibility.Collapsed;
         }
     }
 }
